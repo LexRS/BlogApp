33a434ee1830098c41284d3428c1b94e4e2680ed
Aleksey Poddubnyi <apoddubnyi@axmor.com>
2026-02-03 18:11:42 +0300

    Initial Commit

---
+++ BlogApp/BlogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppApp.swift
+//  BlogApp
+//
+//  Created by Алексей Поддубный on 03.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

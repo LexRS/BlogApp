2597e0f5cf47e373548815aae18cc18a43453363
Aleksey Poddubnyi <apoddubnyi@axmor.com>
2026-02-04 10:45:28 +0300

    Add Post model

---
+++ BlogApp/Models/Post.swift
@@ -0,0 +1,40 @@
+//
+//  Post.swift
+//  BlogApp
+//
+//  Created by Алексей Поддубный on 03.02.2026.
+//
+import Foundation
+
+struct Post: Codable {
+    let id: Int
+    let title: String
+    let content: String
+    let author: String
+    let createdAt: Date
+    let updatedAt: Date
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case content = "content"
+        case author = "author"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
+
+struct PaginatedPosts: Codable {
+    let posts: [Post]
+    let nextCursor: String
+    let prevCursor: String
+    let hasBool: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        case posts = "posts"
+        case nextCursor = "next_cursor"
+        case prevCursor = "prev_cursor"
+        case hasBool = "has_bool"
+    }
+}
+

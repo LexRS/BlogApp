3810d8dcec0d82789c785f7ffe42251d71bd6982
Aleksey Poddubnyi <lex_rs@mail.ru>
2026-03-31 13:13:07 +0300

    MVVM Coordinator architecture transition

--- BlogApp/Networking/Posts/ApiPostsProvider.swift
+++ BlogApp/Networking/Posts/ApiPostsProvider.swift
@@ -9,6 +9,8 @@ import Foundation
 
 protocol ApiPostsProvider: AnyObject {
     func getPosts(cursor: String?) async throws -> PostsResponse
+    func getPost(id: Int) async throws -> Post
+    func createPost(_ post: CreatePostRequest) async throws -> Post
 }
 
 final class DefaultApiPostsProvider: ApiPostsProvider {
@@ -21,6 +23,18 @@ final class DefaultApiPostsProvider: ApiPostsProvider {
     func getPosts(cursor: String? = nil) async throws -> PostsResponse {
         let router = PostsRouter.postsFeed(cursor: cursor)
         let response: PostsResponse = try await apiProvider.request(router)
+        return response
+    }
+    
+    func getPost(id: Int) async throws -> Post {
+        let router = PostsRouter.detailedPost(postID: id)
+        let response: Post = try await apiProvider.request(router)
+        return response
+    }
+    
+    func createPost(_ post: CreatePostRequest) async throws -> Post {
+        let router = PostsRouter.createPost(postRequest: post)
+        let response: Post = try await apiProvider.request(router)
         return response
     }
 }
